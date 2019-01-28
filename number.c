3fb3d30ca6f4f3639f877324e3f77818f3aa68b9
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-01-28 20:22:30 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("number is positive operator");
+    
+    }
+    else
+    {
+        printf("number is negative operator");
+    }
+
+    return 0;
+}
