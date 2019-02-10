30c4c3d6c552203d855badbeb57d741906d9cb22
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-02-10 21:53:25 +0530

    set47.c

---
+++ set47.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ int main(void) 
+{
+int x,y;
+printf("\n enter two numbers to swap:");
+scanf("%d %d",&x,&y);
+printf("\nx=%d",x);
+printf("\ny=%d",y);
+x=x+y;
+y=x-y;
+x=x-y;
+printf("\nx=%d",x);
+printf("\ny=%d",y);
+return 0;
+}
