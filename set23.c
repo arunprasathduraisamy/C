2c92e7ee44235d0c1bfa24765e9509251b874c12
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-02-10 19:34:24 +0530

    set23.c

--- set23.c
+++ set23.c
@@ -3,20 +3,20 @@ int main()
 {
 int n,i;
 scanf("%d",&n);
-int flag=1;
+int f=1;
 for(i=2;i<n/2;i++)
 {
 
     if(n%i==0)
     {
-       flag=0;
+       f=0;
     }
     else
     {
-       flag=1;
+       f=1;
     }
 }
-if(flag==1)
+if(f==1)
 {
     printf("prime");
 }
