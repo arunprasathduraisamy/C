eeba695aad024f6701b848e354b740c0af5522d8
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-02-10 19:37:05 +0530

    set24.c

---
+++ set24.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+    int i,a,b;
+    printf("\nEnter the number\n");
+    scanf("%d%d",&a,&b);
+    for(i=a+1;i<b;i++)
+    {
+        if(i%2==1)
+        {
+            printf("%d\n",i);
+            
+        }
+    }
+}
+
