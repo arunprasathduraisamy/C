afae8d304330a4089f6b1bef8f9cac77dfc1c713
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-02-11 20:17:12 +0530

    set655.c

---
+++ set655.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() 
+
+	int n,m,p;
+	scanf("%d",&n);
+	scanf("%d",&m);
+	p=n*m;
+	if(p%2==0)
+	{
+		printf("\n even");
+	}
+	else
+	{
+		printf("\n odd");
+	}
+	return 0;
+}
+ 
