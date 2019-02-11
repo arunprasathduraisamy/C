d9389b3dc49917f38d48ea9d63d031f6850c69f8
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-02-11 10:17:21 +0530

    set51.c

---
+++ set51.c
@@ -0,0 +1,14 @@
+include<stdio.h>
+#include<string.h>
+int main()
+{
+	int n,i;
+	char s[10];
+	gets(s);
+	printf("enter n ");
+    scanf("%d",&n);
+for(i=0;i<n;i++)
+	{
+	  printf("%s\n",s);   
+ 		}
+}
