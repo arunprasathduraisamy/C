71844ce290a9ae18e4970ae6685e7bcd5c348256
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-02-14 21:30:33 +0530

    set83.c

--- set83.c
+++ set83.c
@@ -1,15 +1,15 @@
 int main(void) 
 {
-	int n,l,r;
-	scanf("%d",&n);
-	scanf("%d %d",&l,&r);
-	if(n>l && n<r)
-	{
-		printf("\n yes");
-	}
-	else
-	{
-		printf("\n no");
-	}
-	return 0;
+int n,left,right;
+scanf("%d",&n);
+scanf("%d %d",&left,&right);
+if(n>left && n<right)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+return 0;
 }
