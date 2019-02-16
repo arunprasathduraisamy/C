cb3148653ac1c19d1cb576cf0c955e0e0f78cda5
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-02-16 20:40:27 +0530

    set111.c

---
+++ set111.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	char str[50];
+	int i,num;
+	scanf("%s%d",str,&n);
+	int len;
+	len=strlen(str);
+	for(i=num;i<=len;i++)
+	{
+		printf("%c",str[i]);
+	}
+return 0;
+}
