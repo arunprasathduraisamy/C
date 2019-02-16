55026fad3d5b8c0d1092388da2a97b320caebe59
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-02-16 20:30:16 +0530

    set108.c

--- set108.c
+++ set108.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
  int main() 
 {
-int n,a[30],i;
-scanf("%d",&n);
-for(i=1;i<=n;i++)
+int num,a[30],i;
+scanf("%d",&num);
+for(i=1;i<=num;i++)
 {
 scanf("%d",&a[i]);
 }
-for(i=1;i<=n;i++)
+for(i=1;i<=num;i++)
 {
 if(i!=a[i])
 {
@@ -15,5 +15,5 @@ printf("\n%d",i);
 break;
 }
 }
- return 0;
+return 0;
 }
