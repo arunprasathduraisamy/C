ce4e7c81a060727fc5800a29af256c4e6e825df5
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-02-16 20:19:23 +0530

    set107.c

---
+++ set107.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+ {
+int n,rem,rev=0;
+int result;
+scanf("%d",&n);
+ while(n!=0)
+ {
+ rem=n%10;
+ rev=rev*10+rem;
+ n=n/10;
+ }
+printf("\n%d",rev);
+return 0;
+ 
+}
