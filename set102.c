ea035b4a45c9cdfa6cb8ceee6a98f9bbbe141aa6
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-02-16 19:55:21 +0530

    set102.c

---
+++ set102.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{
+	int num,a[50],i,sum=0;
+	scanf("%d",&n);
+	for(i=0;i<num;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<num;i++)
+	{
+		sum=sum+a[i];	
+	}
+	printf("\n%d",sum);
+	return 0;
+}
