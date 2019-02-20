6c499ec809db8b8d0c4181b28501f0c8e20e2279
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-02-20 16:06:05 +0530

    ps17.c

---
+++ ps17.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+char a[10],b,i;
+scanf("%s",&a);
+for(i=0;a[i]!='\0';i++)
+{
+    if(i%2==0)
+    {
+        b=a[i];
+        a[i]=a[i+1];
+        a[i+1]=b;
+    }
+}
+printf("%s",a);
+return 0;
+
+}
