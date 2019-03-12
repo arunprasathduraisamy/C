a8c0f1ba89dfd56e6f0867fef83553dd18ef8590
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-03-12 20:00:35 +0530

    ps51.c

---
+++ ps51.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(void) 
+{
+	int n,k,i,j,m=1,f=0;
+	scanf("%d %d",&n,&k);
+	for(i=0;i<n;i++)
+	{
+			for(j=i;j>0;j--)
+			{
+				m=m*k;
+				if(m==n)
+				{
+					f=1;
+					break;
+				}
+			}
+	}
+	if(f==1)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
