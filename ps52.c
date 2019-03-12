1c5751aad13fba3c497ba3d6ad0baf811c9f6496
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-03-12 20:08:36 +0530

    ps52.c

---
+++ ps52.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+    int a[10],i,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+        if(a[i]==n)
+        {
+            printf("no");
+        
+        }
+        else
+        {
+            printf("yes");
+        }
+    }
