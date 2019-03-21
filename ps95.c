adacd1ff79d1b991ebb3dad94541abcadb711956
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-03-21 19:46:39 +0530

    ps95.c

---
+++ ps95.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+	int n;
+	scanf("%d",&n);
+	int array[n],i,r=0;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d ",&array[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		r=(r)|(array[i]);
+	}
+	printf("%d",res);
+	return 0;
+}
