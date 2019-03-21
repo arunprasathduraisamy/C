4bac329d13a099f3687c86809d6fd71279de0822
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-03-21 12:34:27 +0530

    ps67.c

---
+++ ps67.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+	char str[20],c;
+	int i,count=0;
+	scanf("%s %c",str,&c);
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]==c)
+		{
+			count=count+1;
+			
+		}
+	}
+	printf("%d",count);
+	return 0;
+}
