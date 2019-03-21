68ab21d15a4b3994d0f5fb9e6e1f4efed124785a
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-03-21 19:27:57 +0530

    ps878.c

---
+++ ps878.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    int m,a[10],i,n,t,j,k,b[10];
+    scanf("%d%d",&n,&m);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<m;i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    for(i=0;i<m;i++)
+    {
+        a[n]=b[i];
+        n++;
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            
+    if(a[i]>a[j])
+    {
+        temp=a[i];
+        a[i]=a[j];
+        a[j]=temp;
+    }
+        }
+        printf("%d",a[i]);
+    }
+    return 0;
+}
