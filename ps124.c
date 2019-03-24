087e0bccf1db18cf4c90cef578b9f49513f11b13
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-03-24 12:05:18 +0530

    ps124.c

---
+++ ps124.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void main() 
+
+{
+
+	int num,sum=0,sq,rem;
+
+	scanf("%d",&num);
+
+	while(num!=0)
+
+	{
+
+		rem=num%10;
+
+		sq=rem*rem;
+
+		sum=sum+sq;
+
+		num=num/10;
+
+	}
+
+	printf("\n%d",sum);
+}
