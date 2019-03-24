bc38f96f9e46833617662efb62d0b296b08a622d
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-03-24 13:19:30 +0530

    ps155.c

---
+++ ps155.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,f=1,i,an;
+    scanf("%d %d",&a,&b);
+    if(a-b<=5)
+    {
+    for(i=1;i<=a;i++)
+     f=f*i;
+    a=f;
+    f=1;
+    for(i=1;i<=b;i++)
+    f=f*i;
+    b=f;
+    an=a/b;
+    printf("%d",an);
+    }
+  }
