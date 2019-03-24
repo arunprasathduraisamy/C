7a57a09c3faee34c25bca290798d85981a97040b
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-03-24 12:20:55 +0530

    ps1210.c

--- ps1210.c
+++ ps1210.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
 void main() 
-
 {
-
-	int num,sum=0,sq,rem;
-
-	scanf("%d",&num);
-
-	while(num!=0)
-
+int n,sum=0,sq,rem;
+	scanf("%d",&n);
+	while(n!=0)
 	{
-
-		rem=num%10;
-
+		rem=n%10;
 		sq=rem*rem;
-
 		sum=sum+sq;
-
-		num=num/10;
-
+		n=n/10;
 	}
-
 	printf("\n%d",sum);
 }
