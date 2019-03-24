ee6d8f961d81636fcae1a7e4c08f943aa63e1cda
arunprasathduraisamy <36393693+arunprasathduraisamy@users.noreply.github.com>
2019-03-24 12:47:42 +0530

    ps141.c

---
+++ ps141.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main() 
+{
+	int num,rem,osum=0;
+	scanf("%d",&num);
+	while(num!=0)
+	{
+		rem=num%10;
+		if(rem%2!=0)
+		{
+			osum=osum+rem;
+		}
+		num=num/10;
+	}
+	
+	if(osum%2==0)
+	{
+		printf("E");
+	}
+	else
+	{
+		printf("O");
+	}
+}
